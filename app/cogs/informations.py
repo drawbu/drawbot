19bd813f0fac8f4e114049877528055da186f8dd
Yohann Boniface <edhyjox@gmail.com>
2021-06-30 18:45:23 +0200

    adding help command

--- app/cogs/informations.py
+++ app/cogs/informations.py
@@ -1,3 +1,6 @@
+import os.path
+
+import discord
 from discord.ext import commands
 
 
@@ -5,6 +8,18 @@ class Informations(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+
+    @commands.command(
+        name='help', aliases=('h', 'aide'),
+    )
+    async def help_command(self, ctx):
+        help_embed = discord.Embed(
+            title="Help of the Pronote",
+            description="Un bot qui traque vos devoir pronote et vous les notifient sur discord",
+            color=self.client.embed_color
+        )
+
+        await ctx.send(embed=help_embed)
 
 
 def setup(client):
