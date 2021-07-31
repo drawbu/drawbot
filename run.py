46d0867d3208773912cf2a14bfe753c779cb7143
Yohann Boniface <edhyjox@gmail.com>
2021-07-31 23:24:19 +0200

    wrapping run.py into main function

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 from app.bot import Bot
 
-bot = Bot()
-bot.run()
+
+def main() -> None:
+    """Entry point to run the bot client."""
+    bot = Bot()
+    bot.run()
+
+
+if __name__ == '__main__':
+    main()
