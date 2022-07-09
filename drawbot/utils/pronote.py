88c16dc27ecb425baaba96c57c0144cb3d9582ab
sigmanificient <edhyjox@gmail.com>
2022-07-09 06:06:02 +0200

    :recycle: Factorized fetch json

--- drawbot/utils/pronote.py
+++ drawbot/utils/pronote.py
@@ -1,6 +1,7 @@
 from typing import DefaultDict, Generator, Optional
 from collections import defaultdict
 
+from ..json_types import JsonData
 from ..utils import json_wr
 import pronotepy
 
@@ -17,29 +18,7 @@ def fetch_homeworks(pronote_client: pronotepy.Client) -> Optional[Generator]:
             }
         )
 
-    homeworks_file = json_wr("devoirs")
-    if homeworks == homeworks_file:
-        return
-
-    json_wr("devoirs", "w", homeworks)
-
-    homeworks_list: list = []
-    for key, value in homeworks.items():
-        for i in value:
-            i["date"] = key
-        homeworks_list.extend(value)
-
-    homeworks_old_list: list = []
-    for key, value in homeworks_file.items():
-        for i in value:
-            i["date"] = key
-        homeworks_old_list.extend(value)
-
-    for homework in homeworks_list:
-        if homework in homeworks_old_list:
-            continue
-
-        yield homework
+    yield from fetch_from_json("devoirs", homeworks)
 
 
 def fetch_grades(pronote_client: pronotepy.Client) -> Optional[Generator]:
@@ -59,26 +38,31 @@ def fetch_grades(pronote_client: pronotepy.Client) -> Optional[Generator]:
             }
         )
 
-    grades_file = json_wr("grades")
-    if grades == grades_file:
-        return
+    yield from fetch_from_json("grades", grades)
 
-    json_wr("grades", "w", grades)
 
-    grades_list: list = []
-    for key, value in grades.items():
+def fetch_from_json(filename: str, json_data: JsonData):
+    json_file = json_wr(filename)
+
+    if json_data == json_file:
+        yield from ()
+
+    json_wr(filename, "w", json_data)
+
+    json_vals: list = []
+    for key, value in json_data.items():
         for i in value:
             i["date"] = key
-        grades_list.extend(value)
+        json_vals.extend(value)
 
-    grades_old_list: list = []
-    for key, value in grades_file.items():
+    old_vals: list = []
+    for key, value in json_file.items():
         for i in value:
             i["date"] = key
-        grades_old_list.extend(value)
+        old_vals.extend(value)
 
-    for grade in grades_list:
-        if grade in grades_old_list:
+    for value in json_vals:
+        if value in old_vals:
             continue
 
-        yield grade
+        yield value
